e007e9bb2452f12d2fc928171dc86d2014c886a2
Veli-Matti Tuomaala <t8tuve00@students.oamk.fi>
2020-12-10 19:26:03 +0200

    MQTT Receiver program for server. Inserts MQTT messages to specified table.

---
+++ mqtt_to_sql.py
@@ -0,0 +1,39 @@
+import paho.mqtt.client as mqtt
+import time
+import mysql.connector
+
+def insert_data(idUser):
+    sqlcon = mysql.connector.connect(host='localhost',database='lock_info',user='t8tuve00',password='t8tuve00')
+    query = "INSERT INTO Log(idLog,idUser) VALUES(NULL,%s)" %idUser
+    cursor = sqlcon.cursor()
+    cursor.execute(query)
+    sqlcon.commit()
+    cursor.close()
+    sqlcon.close()
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("recog")
+ 
+def on_message(client, userdata, msg):
+      msg.payload = msg.payload.decode("utf-8")
+      print ("\n------------- RECEIVED DATA-------------")
+      print (" MQTT Topic: " + msg.topic )
+      print (" Data: " + str(msg.payload))
+      insert_data(msg.payload)
+
+mqttHost = "broker.hivemq.com"
+mqttPort = 1883
+clientName = "SQLserver"
+client = mqtt.Client(clientName)
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(mqttHost, mqttPort)
+
+print('---------------------------')
+print(' MQTT Receiver Initialized\n Press Ctrl-C to exit')
+
+try:
+    client.loop_forever()
+except KeyboardInterrupt:
+    print(' pressed. Shutting down...')
+    print('---------------------------')
